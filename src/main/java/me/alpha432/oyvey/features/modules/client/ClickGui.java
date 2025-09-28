819ed8e65e5aa62cbc97d417dff3e8f041e2d1bc
IdkGotBoredAsf <pyraclient@gmail.com>
2025-09-28 11:55:32 +0100

    ClickGui.java

--- src/main/java/me/alpha432/oyvey/features/modules/client/ClickGui.java
+++ src/main/java/me/alpha432/oyvey/features/modules/client/ClickGui.java
@@ -9,30 +9,40 @@ import me.alpha432.oyvey.features.modules.Module;
 import me.alpha432.oyvey.features.settings.Setting;
 import org.lwjgl.glfw.GLFW;
 
-public class ClickGui
-        extends Module {
+/**
+ * Modernized ClickGUI module with smoother behavior and cleaner color management.
+ */
+public class ClickGui extends Module {
     private static ClickGui INSTANCE = new ClickGui();
+
+    // General settings
     public Setting<String> prefix = str("Prefix", ".");
-    public Setting<Integer> red = num("Red", 0, 0, 255);
-    public Setting<Integer> green = num("Green", 0, 0, 255);
-    public Setting<Integer> blue = num("Blue", 255, 0, 255);
-    public Setting<Integer> hoverAlpha = num("Alpha", 180, 0, 255);
-    public Setting<Integer> topRed = num("SecondRed", 0, 0, 255);
-    public Setting<Integer> topGreen = num("SecondGreen", 0, 0, 255);
-    public Setting<Integer> topBlue = num("SecondBlue", 150, 0, 255);
-    public Setting<Integer> alpha = num("HoverAlpha", 240, 0, 255);
     public Setting<Boolean> rainbow = bool("Rainbow", false);
-    public Setting<Integer> rainbowHue = num("Delay", 240, 0, 600);
-    public Setting<Float> rainbowBrightness = num("Brightness ", 150.0f, 1.0f, 255.0f);
-    public Setting<Float> rainbowSaturation = num("Saturation", 150.0f, 1.0f, 255.0f);
+
+    // Base colors
+    public Setting<Integer> red = num("Red", 0, 0, 255);
+    public Setting<Integer> green = num("Green", 120, 0, 255);
+    public Setting<Integer> blue = num("Blue", 255, 0, 255);
+    public Setting<Integer> alpha = num("Alpha", 200, 0, 255);
+
+    // Gradient / secondary colors
+    public Setting<Integer> topRed = num("SecondRed", 60, 0, 255);
+    public Setting<Integer> topGreen = num("SecondGreen", 180, 0, 255);
+    public Setting<Integer> topBlue = num("SecondBlue", 255, 0, 255);
+    public Setting<Integer> hoverAlpha = num("HoverAlpha", 240, 0, 255);
+
+    // Rainbow controls
+    public Setting<Integer> rainbowHue = num("HueSpeed", 240, 0, 600);
+    public Setting<Float> rainbowBrightness = num("Brightness", 200f, 1f, 255f);
+    public Setting<Float> rainbowSaturation = num("Saturation", 200f, 1f, 255f);
 
     public ClickGui() {
-        super("ClickGui", "Opens the ClickGui", Module.Category.CLIENT, true, false, false);
+        super("ClickGui", "Opens a modern smoother ClickGUI", Category.CLIENT, true, false, false);
         setBind(GLFW.GLFW_KEY_RIGHT_SHIFT);
         rainbowHue.setVisibility(v -> rainbow.getValue());
         rainbowBrightness.setVisibility(v -> rainbow.getValue());
         rainbowSaturation.setVisibility(v -> rainbow.getValue());
-        this.setInstance();
+        setInstance();
     }
 
     public static ClickGui getInstance() {
@@ -51,30 +61,37 @@ public class ClickGui
         if (event.getStage() == 2 && event.getSetting().getFeature().equals(this)) {
             if (event.getSetting().equals(this.prefix)) {
                 OyVey.commandManager.setPrefix(this.prefix.getPlannedValue());
-                Command.sendMessage("Prefix set to {global} %s", OyVey.commandManager.getPrefix());
+                Command.sendMessage("Prefix set to §b%s", OyVey.commandManager.getPrefix());
             }
-            OyVey.colorManager.setColor(this.red.getPlannedValue(), this.green.getPlannedValue(), this.blue.getPlannedValue(), this.hoverAlpha.getPlannedValue());
+            updateColors();
         }
     }
 
     @Override
     public void onEnable() {
-        if (fullNullCheck()) {
-            return;
-        }
-        mc.setScreen(OyVeyGui.getClickGui());
+        if (fullNullCheck()) return;
+        mc.setScreen(new OyVeyGui(true)); // pass a flag to use new smooth design
     }
 
     @Override
     public void onLoad() {
-        OyVey.colorManager.setColor(this.red.getValue(), this.green.getValue(), this.blue.getValue(), this.hoverAlpha.getValue());
+        updateColors();
         OyVey.commandManager.setPrefix(this.prefix.getValue());
     }
 
     @Override
     public void onTick() {
-        if (!(ClickGui.mc.currentScreen instanceof OyVeyGui)) {
-            this.disable();
+        if (!(mc.currentScreen instanceof OyVeyGui)) {
+            disable();
         }
+    }
+
+    private void updateColors() {
+        OyVey.colorManager.setColor(
+            this.red.getPlannedValue(),
+            this.green.getPlannedValue(),
+            this.blue.getPlannedValue(),
+            this.alpha.getPlannedValue()
+        );
     }
 }
